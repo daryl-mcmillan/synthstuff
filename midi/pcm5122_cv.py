e0b351160573a666414945ef234789d10c27bbd1
Daryl McMillan <daryl.mcmillan@d2l.com>
2026-08-02 15:29:50 -0400

    Update pcm5122_cv.py
    gate and trigger and clock pulses

--- midi/pcm5122_cv.py
+++ midi/pcm5122_cv.py
@@ -67,6 +67,42 @@ def set_note_cv(val):
     dc_memory_buffer[1] = i  # right
 
 
+# set up 2ms pulse pins for clock and trigger
+pulse_code = """
+.program pulse_2ms
+    pull block
+    set pins, 1
+    set x, 9 ; loop for a longer delay
+
+count_loop:
+    jmp x-- count_loop [19]  ; 20 cycle delay
+
+    set pins, 0
+"""
+pulse_program = adafruit_pioasm.assemble(pulse_code)
+def add_pulse_pin( pin ):
+    return rp2pio.StateMachine(
+        pulse_program,
+        frequency=100_000,
+        first_set_pin=pin,
+        set_pin_count=1,
+        initial_out_pin_state=0,
+    )
+
+def send_pulse(sm):
+    array.array('i', [0])
+    sm.write(array.array('i', [0]))
+
+clock_pin = add_pulse_pin(board.GP2)
+clock_24_pin = add_pulse_pin(board.GP3)
+a_trigger_pin = add_pulse_pin(board.GP6)
+b_trigger_pin = add_pulse_pin(board.GP7)
+
+a_gate_pin = digitalio.DigitalInOut(board.GP8)
+a_gate_pin.direction = digitalio.Direction.OUTPUT
+b_gate_pin = digitalio.DigitalInOut(board.GP9)
+b_gate_pin.direction = digitalio.Direction.OUTPUT
+
 # set up midi input
 uart1 = busio.UART(
     tx=board.GP4,
@@ -87,20 +123,31 @@ def process_midi_command():
     if b[0] == 0xF8:
         midi_clock += 1
         # midi clock tick
+        send_pulse( clock_24_pin )
         if midi_clock % 24 == 0:
             # quarter note tick
-            pass
+            send_pulse( clock_pin )
         return
     current_midi_command = (current_midi_command << 8) + b[0]
     if ( current_midi_command & 0x00FF0000 ) == 0x00900000:
-        led.value = True
         set_note_cv( ( current_midi_command >> 8 ) & 0x7F )
+        send_pulse( a_trigger_pin )
+        a_gate_pin.value = True
+        led.value = True
         current_midi_command = 0
         return
     if ( current_midi_command & 0x00FF0000 ) == 0x00800000:
+        a_gate_pin.value = False
         led.value = False
         current_midi_command = 0
         return
 
 while True:
     process_midi_command()
+
+#    set_dc_val(2003238912)   # 2v
+#    time.sleep(0.2)
+#    set_dc_val(1001652224)   # 1v
+#    time.sleep(0.2)
+#    set_dc_val(0)  # 0v
+#    time.sleep(0.2)
