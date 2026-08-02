b538d5280861f3a1f0460f3998993525e194f33e
Daryl McMillan <daryl.mcmillan@d2l.com>
2026-08-02 17:01:31 -0400

    Update pcm5122_cv.py
    two channels

--- midi/pcm5122_cv.py
+++ midi/pcm5122_cv.py
@@ -59,12 +59,12 @@ sm = rp2pio.StateMachine(
 
 sm.background_write(loop=dc_memory_buffer)
 
-def set_note_cv(val):
-    i = (val-64) * 83471018
+def set_note_cv(cv_index, val):
+    semi = val - 69 # A440 = 0
+    i = semi * 41735509
     i = min( 0x7FFFFFFF, max( i, 0 - 0x80000000 ) )
     # this is the DMA buffer
-    dc_memory_buffer[0] = i  # left
-    dc_memory_buffer[1] = i  # right
+    dc_memory_buffer[cv_index] = i
 
 
 # set up 2ms pulse pins for clock and trigger
@@ -95,13 +95,52 @@ def send_pulse(sm):
 
 clock_pin = add_pulse_pin(board.GP2)
 clock_24_pin = add_pulse_pin(board.GP3)
-a_trigger_pin = add_pulse_pin(board.GP6)
-b_trigger_pin = add_pulse_pin(board.GP7)
 
-a_gate_pin = digitalio.DigitalInOut(board.GP8)
-a_gate_pin.direction = digitalio.Direction.OUTPUT
-b_gate_pin = digitalio.DigitalInOut(board.GP9)
-b_gate_pin.direction = digitalio.Direction.OUTPUT
+class OutputChannel:
+    def __init__(self, cv_index, gate_pin, trigger_pin):
+        self.cv_index = cv_index
+        self.gate_pin = digitalio.DigitalInOut(gate_pin)
+        self.gate_pin.direction = digitalio.Direction.OUTPUT
+        self.trigger_pin = add_pulse_pin(trigger_pin)
+
+    def note_on(self, note):
+        set_note_cv( self.cv_index, note )
+        send_pulse( self.trigger_pin )
+        self.gate_pin.value = True
+
+    def note_off(self):
+        self.gate_pin.value = False
+
+class NullOutputChannel:
+    def __init__(self):
+        pass
+    
+    def note_on(self, note):
+        pass
+    
+    def note_off(self):
+        pass
+
+null_output = NullOutputChannel()
+
+output_channels = [
+    OutputChannel( cv_index = 0, gate_pin = board.GP8, trigger_pin = board.GP6 ),
+    OutputChannel( cv_index = 1, gate_pin = board.GP9, trigger_pin = board.GP7 ),
+    null_output,
+    null_output,
+    null_output,
+    null_output,
+    null_output,
+    null_output,
+    null_output,
+    null_output,
+    null_output,
+    null_output,
+    null_output,
+    null_output,
+    null_output,
+    null_output
+]
 
 # set up midi input
 uart1 = busio.UART(
@@ -129,25 +168,18 @@ def process_midi_command():
             send_pulse( clock_pin )
         return
     current_midi_command = (current_midi_command << 8) + b[0]
-    if ( current_midi_command & 0x00FF0000 ) == 0x00900000:
-        set_note_cv( ( current_midi_command >> 8 ) & 0x7F )
-        send_pulse( a_trigger_pin )
-        a_gate_pin.value = True
+    if ( current_midi_command & 0x00F00000 ) == 0x00900000:
+        channel = ( current_midi_command & 0x000F0000 ) >> 16
+        output_channels[channel].note_on( ( current_midi_command >> 8 ) & 0x7F )
         led.value = True
         current_midi_command = 0
         return
-    if ( current_midi_command & 0x00FF0000 ) == 0x00800000:
-        a_gate_pin.value = False
+    if ( current_midi_command & 0x00F00000 ) == 0x00800000:
+        channel = ( current_midi_command & 0x000F0000 ) >> 16
+        output_channels[channel].note_off()
         led.value = False
         current_midi_command = 0
         return
 
 while True:
     process_midi_command()
-
-#    set_dc_val(2003238912)   # 2v
-#    time.sleep(0.2)
-#    set_dc_val(1001652224)   # 1v
-#    time.sleep(0.2)
-#    set_dc_val(0)  # 0v
-#    time.sleep(0.2)
